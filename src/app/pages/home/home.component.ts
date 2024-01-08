ba063f4732835251f562e4e5b950b77882a2e50c
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2024-01-08 23:39:49 +0100

    feat: add dynamic offers rendering, fix navbar z-index

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,21 @@
-import {ChangeDetectorRef, Component, inject, NgZone} from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import { ChangeDetectorRef, Component, inject, NgZone } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { OfferCardComponent } from '../../components/offer-card/offer-card.component';
+import { OfferInterface, SZ } from '../../core/interfaces/offer.interface';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    NgOptimizedImage
-  ],
+  imports: [NgOptimizedImage, OfferCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   welcomeImgSrc = 'assets/a.jpg';
+  offers: OfferInterface[] = [SZ];
   i: number = 2;
-  ngZone = inject(NgZone)
-  cd=inject(ChangeDetectorRef)
+  ngZone = inject(NgZone);
+  cd = inject(ChangeDetectorRef);
 
   constructor() {
     // afterNextRender(()=>{
@@ -24,15 +25,10 @@ export class HomeComponent {
     // })
   }
 
-
   animateWelcomeImage() {
-    if(this.i % 2)
-      this.welcomeImgSrc='assets/a.jpg'
-    else
-      this.welcomeImgSrc='assets/bg.jpg'
+    if (this.i % 2) this.welcomeImgSrc = 'assets/a.jpg';
+    else this.welcomeImgSrc = 'assets/bg.jpg';
     this.i++;
-    this.cd.detectChanges()
+    this.cd.detectChanges();
   }
-
-
 }
